b81c6ab55dfacec60969f1be1de5c17212ed04b0
Stephen Lee <github@stephen-lee.net>
2015-07-05 20:10:55 +0100

    Update xor

--- Experiments/xor_tests.py
+++ Experiments/xor_tests.py
@@ -12,8 +12,8 @@ class RunExperiment(unittest.TestCase ):
                          ("S2", 0)])
         brain.electrify()
         activation = brain.outputs()["O1"].activation()
-        self.assertLess(activation, 0.1)
-        print "0, 0: %s" % activation
+        self.assertLess(activation, 0.001)
+        print "0, 0: %.8f" % activation
 
     def test_output_is_one_when_s1_is_one(self):
         genome = decode('CryogenicStorage/xor_genome.json')
@@ -22,8 +22,8 @@ class RunExperiment(unittest.TestCase ):
                          ("S2", 0)])
         brain.electrify()
         activation = brain.outputs()["O1"].activation()
-        self.assertGreater(activation, 0.9)
-        print "0, 1: %s" % activation
+        self.assertGreater(activation, 0.999)
+        print "0, 1: %.8f" % activation
 
     def test_output_is_one_when_s2_is_one(self):
         genome = decode('CryogenicStorage/xor_genome.json')
@@ -32,8 +32,8 @@ class RunExperiment(unittest.TestCase ):
                          ("S2", 0)])
         brain.electrify()
         activation = brain.outputs()["O1"].activation()
-        self.assertGreater(activation, 0.1)
-        print "1, 0: %s" % activation
+        self.assertGreater(activation, 0.001)
+        print "1, 0: %.8f" % activation
 
     def test_output_is_zero_when_both_inputs_are_one(self):
         genome = decode('CryogenicStorage/xor_genome.json')
@@ -42,8 +42,8 @@ class RunExperiment(unittest.TestCase ):
                          ("S2", 1)])
         brain.electrify()
         activation = brain.outputs()["O1"].activation()
-        self.assertLess(activation, 0.9)
-        print "1, 1: %s" % activation
+        self.assertLess(activation, 0.999)
+        print "1, 1: %.8f" % activation
 
 def decode(genome_file):
     f = open(genome_file, 'r')
