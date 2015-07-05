5a448d10ea59ec7f203f6d8999757bb1773225e2
Stephen Lee <github@stephen-lee.net>
2015-07-05 20:06:29 +0100

    Add lineage details

--- CommandBunker/LineageTracer.py
+++ CommandBunker/LineageTracer.py
@@ -21,7 +21,12 @@ def main():
         target_id = target.get("parent", "")
         generation = target.get("generation", 0)
 
-    print json.dumps(sorted(lineage, key=lambda o: o.get("generation", 0)))
+    sorted_lineage = sorted(lineage, key=lambda o: o.get("generation", 0))
+
+    for gene in sorted_lineage:
+        print "%s|%s" % (int(gene.get("generation", 0)) + 1, gene["fitness"])
+
+    print json.dumps(sorted_lineage)
 
 
 if __name__ == "__main__":
